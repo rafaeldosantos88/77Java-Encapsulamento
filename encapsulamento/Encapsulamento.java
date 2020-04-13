29c3af78443379e78a9eea601930c95f692a319a
Rafael Dos Santos <raphael_979@hotmail.com>
2020-04-12 20:13:31 -0400

    Encapsulamento

---
+++ encapsulamento/Encapsulamento.java
@@ -0,0 +1,49 @@
+package encapsulamento;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Encapsulamento {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+
+		System.out.println("Enter product data: ");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Price: ");
+		double price = sc.nextDouble();
+		
+
+		Product product = new Product(name,price);
+		
+		product.setName("computer");//Aqui pude mudar o valor com setname
+		System.out.println("Update name:" + product.getName());
+		product.setPrice(1200.00);
+		System.out.println("Update price:" + product.getPrice());
+		
+		
+		System.out.println();
+		System.out.println("Product data: " + product);
+		System.out.println();
+		System.out.print("Enter the number of products to be added in stock: ");
+		 int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		System.out.println();
+		System.out.print("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		sc.close();
+	}
+}
